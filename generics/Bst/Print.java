5d3950e036a435c316eb0444f9b987d15371748c
harish124 <33428138+harish124@users.noreply.github.com>
2018-10-16 08:11:47 +0530

    Add files via upload

---
+++ generics/Bst/Print.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package generics;
+
+/**
+ *
+ * @author Hi
+ */
+public class Print {
+    void print(Object o)
+    {
+        System.out.println(""+o);
+    }
+}
